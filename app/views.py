9d4930fff013b40c18fd235a22dd77648656b857
ssst0n3 <ssst0n3@gmail.com>
2016-06-02 19:10:33 +0800

    项目架构完善
    1.增加了article
    2.增加了后台

--- app/views.py
+++ app/views.py
@@ -6,17 +6,41 @@ sys.setdefaultencoding('utf8')
 
 from flask import render_template
 from app import app
+
+# 首页
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    return 'Hello, World! This is a index page! But our designer have not give me the index page! <br> <a href="xxgg">信息公告</a> &emsp; <a href="xygk">学院概况</a> &emsp; <a href="admin">后台管理</a>'
 
+# 信息公告页
 @app.route('/xxgg/')
 def xxgg():
     pagename = "信息公告"
     return render_template("xinxigonggao.html", pagename = pagename)
 
+# 学院概况页
 @app.route('/xygk/')
 def xygk():
     pagename = "学院概况"
     return render_template("xueyuangaikuang.html", pagename = pagename)
+
+# 文章模板
+@app.route('/article/')
+@app.route('/article/<int:id>')
+def article(id=None):
+    # 数据库操作，返回title， content等信息
+    if id:
+        title = "我是一篇id为" + str(id) + "的文章的标题"
+        content = "我是一篇id为" + str(id) + "的文章的内容"
+        return render_template('article.html', title=title, content=content)
+    else:
+        return 'illegal access'
+
+# 后台管理路由
+@app.route('/admin/')
+def admin():
+    return '后台管理界面'
+
+# 检索功能由第三方提供
+# 评论、分享功能由多说提供
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #-*- coding:utf-8 -*-
 
 from app import app
-app.run(debug = True, port = 5000)
+app.run(debug = True, port = 8080, host="0.0.0.0")
